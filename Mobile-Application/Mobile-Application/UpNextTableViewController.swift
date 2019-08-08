33f2ce64aec1ab3834f49bec01748a94d56954f6
ElisabettaFerreri <elisabetta.ferreri@mail.polimi.it>
2019-08-08 12:07:00 +0200

    rounded edges + background (up next view)

--- Mobile-Application/Mobile-Application/UpNextTableViewController.swift
+++ Mobile-Application/Mobile-Application/UpNextTableViewController.swift
@@ -12,12 +12,14 @@ import UIKit
 class UpNextTableViewController : UITableViewController {
 	
 	var weeks = [String]()
+	let cellSpacingHeight: CGFloat = 15
     
     var tableViewData = [cellData]()
 	
 	override func viewDidLoad() {
 		
 		super.viewDidLoad()
+		
         tableViewData = [cellData(opened: false, title: "Title1", sectionData: ["Cell1", "Cell2", "Cell3"]),
                          cellData(opened: false, title: "Title2", sectionData: ["Cell1", "Cell2", "Cell3"]),
                          cellData(opened: false, title: "Title3", sectionData: ["Cell1", "Cell2", "Cell3"]),
@@ -43,6 +45,10 @@ class UpNextTableViewController : UITableViewController {
         }
 	}
 	
+	override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+		return cellSpacingHeight
+	}
+	
 	override func tableView(_ tableView: UITableView,
 							cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -51,12 +57,16 @@ class UpNextTableViewController : UITableViewController {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "UpNextTableViewCell") else { return UITableViewCell()}
             cell.textLabel?.text = tableViewData[indexPath.section].title
             cell.textLabel?.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.headline)
+			cell.layer.cornerRadius = 10
+			cell.clipsToBounds = true
             return cell
         } else {
             //            Use different cell identifier if needed
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "UpNextTableViewCell") else { return UITableViewCell()}
             cell.textLabel?.text = tableViewData[indexPath.section].sectionData[dataIndex]
             cell.textLabel?.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.headline)
+			cell.layer.cornerRadius = 10
+			cell.clipsToBounds = true
             return cell
         }
 	}
