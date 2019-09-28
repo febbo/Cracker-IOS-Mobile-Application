d2216a38a8c613d16774a26d3ccf778c09918bd0
ElisabettaFerreri <elisabetta.ferreri@mail.polimi.it>
2019-09-28 16:31:43 +0200

    buttons in single issue

--- Mobile-Application/Mobile-Application/Controller/IssueViewController.swift
+++ Mobile-Application/Mobile-Application/Controller/IssueViewController.swift
@@ -14,12 +14,47 @@ class IssueViewController: UIViewController {
 //
 //    var issueName : String?
     
-    override func viewDidLoad() {
+	//TODO: quando avremo il profilo, utilizzeremo il valore che salviamo noi
+	var isRead = false
+	
+	@IBOutlet weak var readButton: UIButton!
+	@IBOutlet weak var seriesButton: UIButton!
+	
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
+		
 		navigationItem.largeTitleDisplayMode = .never
         
+		if (isRead) {
+			readButton.backgroundColor = UIColor(named: "DarkGreen")
+			readButton.setTitle("MARK AS UNREAD", for: .normal)
+		}
+		else {
+			readButton.backgroundColor = UIColor(named: "Red")
+			readButton.setTitle("MARK AS READ", for: .normal)
+		}
+		
+		readButton.addTarget(self, action: #selector(readIssueButton), for: .touchUpInside)
+		seriesButton.addTarget(self, action: #selector(goToSeriesButton), for: .touchUpInside)
+
 //        lblIssue.text = "\((issueName)!)"
     }
 	
+	@objc func readIssueButton(button: UIButton) {
+		isRead = !isRead
+		if (isRead) {
+			readButton.backgroundColor = UIColor(named: "DarkGreen")
+			readButton.setTitle("MARK AS UNREAD", for: .normal)
+		}
+		else {
+			readButton.backgroundColor = UIColor(named: "Red")
+			readButton.setTitle("MARK AS READ", for: .normal)
+		}
+	}
+
+	@objc func goToSeriesButton(button: UIButton) {
+		self.performSegue(withIdentifier: "goToSeries", sender: self)
+	}
 
 }
